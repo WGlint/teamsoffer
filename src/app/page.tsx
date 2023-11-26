1116b828043a280c7d2e4f990217fa7990208dd2
Azdin Mediri <a.elmediri@gmail.com>
2023-11-26 03:25:10 +0100

    Reduction Price Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+"use client"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { useEffect, useState } from 'react'
+
+interface reduction{
+  WithReduction : number
+  WithoutReduction : number
+  is_reduced : boolean
+}
 
 export default function Home() {
+  const [Neople, setNeople] = useState<number>(1)
+  const [PriceWithReduction, setPriceWithReduction] = useState<number>()
+  const [PriceWithoutReduction, setPriceWithoutReduction] = useState<number>()
+  const [isReduced, setIsReduced] = useState<boolean>(false)
+
+  const LessP = () => {
+    if (Neople != 0){
+      setNeople(Neople - 1)
+    }
+  }
+
+  const HighP = () => {
+    setNeople(Neople + 1)
+  }
+
+  useEffect(() => {
+    function TeamsPrice(
+      number_of_teams: number, // Number of people in teams choice by user
+      price_by_person: number = 39, // Price by person without reduction ( e.g. 39 $ like HeadShotPro)
+      reduction_pourcentage: number[] = [0.8, 0.7, 0.5], // take 3 reduction pourcentage ( e.g. [0.8, 0.7, 0.5] like HeadShotPro)
+      reduction_echelon: number[] = [5, 10, 50] // take 3 reduction echelon ( e.g. [5, 10, 50] like HeadShotPro)
+    ) : reduction{
+      if( number_of_teams < reduction_echelon[0] ){
+        return {
+          WithReduction : parseFloat((number_of_teams * price_by_person).toFixed(2)),
+          WithoutReduction : parseFloat((number_of_teams * price_by_person).toFixed(2)),
+          is_reduced : false
+        }
+      } else if ( number_of_teams < reduction_echelon[1] ){
+        return {
+          WithReduction : parseFloat((number_of_teams * price_by_person * reduction_pourcentage[0]).toFixed(2)),
+          WithoutReduction : parseFloat((number_of_teams * price_by_person).toFixed(2)),
+          is_reduced : true
+        }
+      } else if ( number_of_teams < reduction_echelon[2] ){
+        return {
+          WithReduction : parseFloat((number_of_teams * price_by_person * reduction_pourcentage[1]).toFixed(2)),
+          WithoutReduction : parseFloat((number_of_teams * price_by_person).toFixed(2)),
+          is_reduced : true
+        }
+      } else {
+        return {
+          WithReduction : parseFloat((number_of_teams * price_by_person * reduction_pourcentage[2]).toFixed(2)),
+          WithoutReduction : parseFloat((number_of_teams * price_by_person).toFixed(2)),
+          is_reduced : true
+        }
+      }
+    }
+    setPriceWithReduction(TeamsPrice(Neople).WithReduction)
+    setPriceWithoutReduction(TeamsPrice(Neople).WithoutReduction)
+    setIsReduced(TeamsPrice(Neople).is_reduced)
+  }, [Neople])
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='middle w-[1000px] flex justify-center items-center gap-10 flex-col'>
+      <div className='flex flex-row gap-5'>
+          <Button variant={"outline"} size="icon" onClick={LessP}>
+            <ChevronLeft className='h-4 w-4' />
+          </Button>
+          
+          <Input 
+            placeholder='Number people in teams'
+            value={Neople}
+            className='w-[200px] text-center'
+            onChange={(e) => setNeople(parseInt(e.target.value || '0'))}
+          />
+
+          <Button variant={"outline"} size="icon" onClick={HighP}>
+            <ChevronRight className='h-4 w-4' />
+          </Button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='border rounded-xl text-center px-[150px] py-[20px] flex flex-col gap-4'>
+        {
+          isReduced ? <p className='text-2xl line-through'>{PriceWithoutReduction} $</p> : null
+        }
+        <p className={`text-2xl ${isReduced ? "text-blue-500" : "text-red-500"}`}>{PriceWithReduction} $</p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
